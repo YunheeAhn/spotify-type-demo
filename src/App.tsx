a04f640a4f3fa2a053414b17e819bb26016b27e5
YunheeAhn <ayh95944674@gmail.com>
2025-12-21 20:03:58 +0900

    chore : App.tsx 파일 초기 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from "react-router";
+import "./App.css";
+import AppLayout from "./layout/AppLayout";
+import HomePage from "./pages/HomePage/HomePage";
+
+// 리액트-타입스크립트 스포티파이 페이지 구성목록
+// 0. 사이드바(플레이리스트, 메뉴)
+// 1. 홈페이지(랜딩) '/' <HomePage />
+// 2. 검색 페이지 '/search' <SearchPage />
+// 3. 검색 후 결과 페이지 '/search/:keyword' <SearchResultPage />
+// 4. 플레이리스트 디테일 페이지(사이드바 플레이리스트 클릭시 이동) '/playlist/:id' <PlaylistDetailPage />
+// 5. (모바일) 플레이리스트 보여주는 페이지 '/playlist' <LibraryPage />
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<AppLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="search" element={<div>검색 페이지</div>} />
+        <Route path="search/:keyword" element={<div>검색 결과 페이지</div>} />
+        <Route path="playlist/:id" element={<div>플레이리스트 디테일 페이지</div>} />
+        {/* <Route path="playlist" element={<div>모바일 플레이리스트 페이지</div>} /> */}
+      </Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
