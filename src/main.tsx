3c1386b914ee8b46e75c8aae94061bdcd592d81f
YunheeAhn <ayh95944674@gmail.com>
2025-12-22 16:29:54 +0900

    chore : theme 파일 적용

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,18 @@ import "./index.css";
 import { BrowserRouter } from "react-router";
 import App from "./App";
 
+// MUI 테마 적용을 위한 ThemeProvider 추가
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import theme from "./theme";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline: MUI에서 제공하는 기본 CSS 리셋 및 글로벌 스타일 적용 */}
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </StrictMode>
 );
