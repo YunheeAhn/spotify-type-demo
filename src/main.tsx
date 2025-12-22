5fd519218cbfecb47217d1d906b06d46e12834a8
YunheeAhn <ayh95944674@gmail.com>
2025-12-23 02:40:46 +0900

    chore : 리액트쿼리 연결

--- src/main.tsx
+++ src/main.tsx
@@ -8,14 +8,23 @@ import App from "./App";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import theme from "./theme";
 
+// React Query 설정
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline: MUI에서 제공하는 기본 CSS 리셋 및 글로벌 스타일 적용 */}
-        <CssBaseline />
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          {/* CssBaseline: MUI에서 제공하는 기본 CSS 리셋 및 글로벌 스타일 적용 */}
+          <CssBaseline />
+          <App />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </ThemeProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>
 );
