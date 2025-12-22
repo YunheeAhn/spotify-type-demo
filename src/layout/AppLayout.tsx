5d997bf317fc015265a8fe8e8a3fda8d85e04b40
YunheeAhn <ayh95944674@gmail.com>
2025-12-22 20:00:10 +0900

    style : 사이드바 UI 구현

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -1,13 +1,114 @@
 import React from "react";
-import { Outlet } from "react-router";
-
+import { NavLink, Outlet } from "react-router";
+import { styled } from "@mui/material/styles";
+import { Box, Typography } from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import LibraryHeader from "./components/LibraryHeader";
+import Library from "./components/Library";
 const AppLayout = () => {
   return (
-    <div>
-      <h1>Sidebar</h1>
-      <Outlet />
-    </div>
+    <Layout>
+      {/* Sidebar */}
+      <Sidebar>
+        <ContentsBox>
+          <NavList>
+            <NavItem to="/">
+              <HomeIcon />
+              <Typography variant="h2" fontWeight={400}>
+                Home
+              </Typography>
+            </NavItem>
+            <NavItem to="/search">
+              <SearchOutlinedIcon />
+              <Typography variant="h2" fontWeight={400}>
+                Search
+              </Typography>
+            </NavItem>
+          </NavList>
+        </ContentsBox>
+
+        <ContentsBox height={"100%"}>
+          <LibraryHeader />
+          <Library />
+        </ContentsBox>
+      </Sidebar>
+
+      {/* Main Content */}
+      <MainContent>
+        <Outlet />
+      </MainContent>
+    </Layout>
   );
 };
 
 export default AppLayout;
+
+// **스타일 컴포넌트**
+// 레이아웃
+const Layout = styled("div")({
+  display: "flex",
+  height: "100vh",
+  overflow: "hidden",
+  padding: "8px",
+  boxSizing: "border-box",
+});
+
+// 사이드바
+const Sidebar = styled("div")(({ theme }) => ({
+  width: "331px",
+  height: "100%",
+  display: "flex",
+  flexDirection: "column",
+  [theme.breakpoints.down("sm")]: {
+    display: "none",
+  },
+}));
+
+// 사이드바 컨텐츠 박스
+const ContentsBox = styled(Box)(({ theme }) => ({
+  borderRadius: "8px",
+  backgroundColor: theme.palette.background.paper,
+  color: theme.palette.text.primary,
+  width: "100%",
+  padding: "8px",
+  marginBottom: "8px",
+  marginRight: "8px",
+}));
+
+// 내비게이션 리스트
+const NavList = styled("ul")({
+  listStyle: "none",
+  padding: 0,
+  margin: 0,
+});
+
+// 내비게이션 아이템(링크)
+const NavItem = styled(NavLink)(({ theme }) => ({
+  textDecoration: "none",
+  display: "flex",
+  alignItems: "center",
+  gap: "20px",
+  color: theme.palette.text.secondary,
+  transition: "all .3s ease",
+  "&:hover": {
+    color: theme.palette.text.primary,
+  },
+  "&.active": {
+    color: theme.palette.text.primary,
+    fontWeight: "bold",
+  },
+  "& + &": {
+    marginTop: "10px",
+  },
+}));
+
+// 메인 컨텐츠
+const MainContent = styled("div")(({ theme }) => ({
+  width: "calc(100% - 331px)",
+  height: "100%",
+  overflowY: "auto",
+  [theme.breakpoints.down("sm")]: {
+    width: "100%",
+  },
+}));
