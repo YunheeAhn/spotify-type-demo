710faad0bb1f37d651000734e46f4bbf1c0c69ed
YunheeAhn <ayh95944674@gmail.com>
2025-12-24 00:41:46 +0900

    feat : spotify 클라리언트 id, 시크릿키 설정

---
+++ src/configs/authConfig.ts
@@ -0,0 +1,4 @@
+// Spotify API 인증 관련 설정
+// 클라이언트 ID와 시크릿 키를 환경 변수에서 가져옴
+export const clientId = import.meta.env.VITE_SPOTIFY_CLIENT_ID as string;
+export const clientSecret = import.meta.env.VITE_SPOTIFY_CLIENT_SECRET as string;
