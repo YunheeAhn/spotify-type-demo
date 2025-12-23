da2aa05a3e44905d5c6396ad0171b2eba1eafbb3
YunheeAhn <ayh95944674@gmail.com>
2025-12-24 00:42:59 +0900

    feat : 아티스트 응답 타입 설정

---
+++ src/models/artist.ts
@@ -0,0 +1,11 @@
+import { ExternalURLs } from "./commonType";
+
+// 아티스트 타입
+export interface Artists {
+  external_urls?: ExternalURLs;
+  href?: string;
+  id?: string;
+  name?: string;
+  type?: string;
+  uri?: string;
+}
