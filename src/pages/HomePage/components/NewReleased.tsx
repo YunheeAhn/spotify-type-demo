b9ced26b2b244c21691b641ad77bebcad39a47cc
YunheeAhn <ayh95944674@gmail.com>
2025-12-25 23:14:58 +0900

    feat : 새로운 발매 앨범 api 연결 UI

--- src/pages/HomePage/components/NewReleased.tsx
+++ src/pages/HomePage/components/NewReleased.tsx
@@ -1,6 +1,9 @@
 import { Typography } from "@mui/material";
-import { Section } from "../../../common/styles/commonStyled";
+import { AlbumGrid, Section } from "../../../common/styles/commonStyled";
 import useGetNewReleased from "../../../hooks/useGetNewReleased";
+import LoadingSpinner from "../../../common/components/LoadingSpinner";
+import ErrorMessage from "../../../common/components/ErrorMessage";
+import AlbumCard from "../../../common/components/Card";
 
 // 새로운 발매 앨범 섹션 컴포넌트
 const NewReleased = () => {
@@ -9,11 +12,28 @@ const NewReleased = () => {
 
   console.log("New Released Albums Data: ", data);
 
+  if (isLoading) {
+    return <LoadingSpinner />;
+  }
+
+  if (error) {
+    return <ErrorMessage message={error.message} />;
+  }
+
   return (
     <Section>
       <Typography variant="h1" paddingBottom="8px">
         New Released Albums
       </Typography>
+      {data && data.albums.items.length > 0 ? (
+        <AlbumGrid>
+          {data.albums.items.map((album) => (
+            <AlbumCard key={album.id} album={album} />
+          ))}
+        </AlbumGrid>
+      ) : (
+        <Typography variant="h2">No Data</Typography>
+      )}
     </Section>
   );
 };
