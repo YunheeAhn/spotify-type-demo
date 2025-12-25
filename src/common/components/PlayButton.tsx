5ef71e1f1f90f7c382f4ac1e966de9f696ab780b
YunheeAhn <ayh95944674@gmail.com>
2025-12-25 23:13:09 +0900

    feat : 재생버튼 컴포넌트

---
+++ src/common/components/PlayButton.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { styled } from "@mui/material";
+
+const PlayButton = () => {
+  return (
+    <PlayButtonContainer className="play-button">
+      <svg
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M8 5v14l11-7z" fill="black" />
+      </svg>
+    </PlayButtonContainer>
+  );
+};
+
+export default PlayButton;
+
+const PlayButtonContainer = styled("button")(({ theme }) => ({
+  backgroundColor: theme.palette.primary.main,
+  border: "none",
+  borderRadius: "50%",
+  width: "50px",
+  height: "50px",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  cursor: "pointer",
+  boxShadow: "rgba(0, 0, 0, 0.3) 0px 8px 8px 0px",
+  "&:focus": {
+    outline: "none",
+  },
+}));
