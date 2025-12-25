11518bb7eb69dc096a95039bf61039aaa20b7b0e
YunheeAhn <ayh95944674@gmail.com>
2025-12-25 21:20:58 +0900

    feat : 새로운 발매 앨범 데이터 가져오기

--- src/pages/HomePage/components/NewReleased.tsx
+++ src/pages/HomePage/components/NewReleased.tsx
@@ -1,8 +1,14 @@
-import React from "react";
 import { Typography } from "@mui/material";
 import { Section } from "../../../common/styles/commonStyled";
+import useGetNewReleased from "../../../hooks/useGetNewReleased";
 
+// 새로운 발매 앨범 섹션 컴포넌트
 const NewReleased = () => {
+  // 새로운 발매 앨범 데이터 가져오기
+  const { data, error, isLoading } = useGetNewReleased();
+
+  console.log("New Released Albums Data: ", data);
+
   return (
     <Section>
       <Typography variant="h1" paddingBottom="8px">
