85bcef2155b0a57290cf7c707af51ba55c461fb4
YunheeAhn <ayh95944674@gmail.com>
2025-12-25 23:13:55 +0900

    style : 공통 스타일 컴포넌트 반응형 추가

--- src/common/styles/commonStyled.tsx
+++ src/common/styles/commonStyled.tsx
@@ -1,4 +1,5 @@
 import { styled } from "@mui/material";
+import theme from "../../theme";
 
 // 각페이지를 감싸는 컨테이너
 export const Container = styled("div")({
@@ -10,5 +11,25 @@ export const Container = styled("div")({
 export const Section = styled("div")({
   "& + &": {
     marginTop: "20px",
+  },
+
+  [theme.breakpoints.down("md")]: {
+    "& + &": { marginTop: "16px" },
+  },
+  [theme.breakpoints.down("sm")]: {
+    "& + &": { marginTop: "12px" },
+  },
+});
+
+// 앨범 카드들을 감싸는 그리드 컴포넌트
+export const AlbumGrid = styled("div")({
+  display: "flex",
+  flexWrap: "wrap",
+  gap: "20px",
+  [theme.breakpoints.down("lg")]: {
+    gap: "16px",
+  },
+  [theme.breakpoints.down("sm")]: {
+    gap: "12px",
   },
 });
