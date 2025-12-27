33b0923fb7de799a2c9121dbca7ba3b8971ea3fd
YunheeAhn <ayh95944674@gmail.com>
2025-12-27 23:40:39 +0900

    feat : 리다이렉트 URI 추가

--- src/configs/commonConfig.ts
+++ src/configs/commonConfig.ts
@@ -1,1 +1,6 @@
+// export const SPOTIFY_BASE_URL = import.meta.env.VITE_SPOTIFY_BASE_URL as string;
+// export const REDIRECT_URI = import.meta.env.VITE_REDIRECT_URI as string;
+
 export const SPOTIFY_BASE_URL = "https://api.spotify.com/v1";
+
+export const REDIRECT_URI = "http://127.0.0.1:5173/callback";
