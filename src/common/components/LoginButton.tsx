68e61a03182be4f332b47d060ae5ac9013154c6b
YunheeAhn <ayh95944674@gmail.com>
2025-12-27 23:40:20 +0900

    feat : spotify 로그인 로직 추기

--- src/common/components/LoginButton.tsx
+++ src/common/components/LoginButton.tsx
@@ -1,9 +1,16 @@
-import { Button } from "@mui/material";
 import React from "react";
+import { Button } from "@mui/material";
+import { getSpotifyAuthUrl } from "../../utils/authSpotify";
 
+// 로그인 컴포넌트
 const LoginButton = () => {
+  // 로그인 처리 함수
+  const login = () => {
+    getSpotifyAuthUrl(); // 스포티파이 인증 URL로 리다이렉트
+  };
+
   return (
-    <Button variant="contained" color="secondary" size="large">
+    <Button variant="contained" color="secondary" size="large" onClick={login}>
       Login
     </Button>
   );
