e0cfe8d3ab790f7f55257436800e7f70991c16cc
YunheeAhn <ayh95944674@gmail.com>
2025-12-28 00:33:05 +0900

    feat : 토큰 교환 api 함수 추가

--- src/apis/authApi.ts
+++ src/apis/authApi.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
-import { client_Id, client_Secret } from "../configs/authConfig";
-import type { ClientCredentialResponse } from "../models/auth";
+import { client_Id, client_Secret, REDIRECT_URI } from "../configs/authConfig";
+import type { ClientCredentialResponse, TokenExchangeResponse } from "../models/auth";
 
 // Base64 인코딩 함수
 const encodedCredentials = btoa(`${client_Id}:${client_Secret}`);
@@ -22,5 +22,33 @@ export const getClientCredentialToken = async (): Promise<ClientCredentialRespon
     return response.data;
   } catch (error) {
     throw new Error("Failed to fetch client credential token");
+  }
+};
+
+// 토큰 교환 함수
+export const exchangeToken = async (
+  code: string,
+  codeVerifier: string
+): Promise<TokenExchangeResponse> => {
+  try {
+    const url = "https://accounts.spotify.com/api/token";
+    if (!client_Id || !REDIRECT_URI) {
+      throw new Error("Missing client ID or redirect URI");
+    }
+    const body = new URLSearchParams({
+      client_id: client_Id,
+      grant_type: "authorization_code",
+      code,
+      redirect_uri: REDIRECT_URI,
+      code_verifier: codeVerifier,
+    });
+    const response = await axios.post(url, body, {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    });
+    return response.data;
+  } catch (error) {
+    throw new Error("Failed to exchange token");
   }
 };
