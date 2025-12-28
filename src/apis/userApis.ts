b34ad52dc8e20a33e1e985c5e9c3c419623574cb
YunheeAhn <ayh95944674@gmail.com>
2025-12-29 01:16:34 +0900

    feat : 현재 사용자(로그인) 프로필 가져오는 함수

---
+++ src/apis/userApis.ts
@@ -0,0 +1,13 @@
+import api from "../utils/api";
+import type { User } from "../models/user";
+
+// 현재 사용자 프로필을 가져오는 함수(API)
+export const getCurrentUserProfile = async (): Promise<User> => {
+  try {
+    const response = await api.get(`/me`);
+
+    return response.data;
+  } catch (error) {
+    throw new Error("Failed to fetch current user profile");
+  }
+};
