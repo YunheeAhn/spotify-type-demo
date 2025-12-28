9be96aacd2c593bfa2005d027a7881aad2d25e43
YunheeAhn <ayh95944674@gmail.com>
2025-12-29 01:17:44 +0900

    feat : 토큰 교환시 현재 프로필 api 함수 공유 로직 추가

--- src/hooks/useExchangeToken.ts
+++ src/hooks/useExchangeToken.ts
@@ -1,13 +1,18 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { exchangeToken } from "../apis/authApi";
 import { TokenExchangeResponse } from "../models/auth";
 
 // useExchangeToken 훅
 const useExchangeToken = () => {
+  const queryClient = useQueryClient();
+
   return useMutation<TokenExchangeResponse, Error, { code: string; codeVerifier: string }>({
     mutationFn: ({ code, codeVerifier }) => exchangeToken(code, codeVerifier),
     onSuccess: (data) => {
       localStorage.setItem("access_token", data.access_token);
+      queryClient.invalidateQueries({
+        queryKey: ["current-User-Profile"],
+      });
     },
   });
 };
