c7ef3dd1ef6ac61ecf7b4be1545d35e66ecc9985
YunheeAhn <ayh95944674@gmail.com>
2025-12-29 18:52:14 +0900

    feat : 로그인 한 현재 유저의 플레이 리스트 가져오는 훅 생성 + 무한 스크롤위한 쿼리

---
+++ src/hooks/useGetCurrentUserPlayList.ts
@@ -0,0 +1,27 @@
+// 로그인 한 현재 유저의 플레이 리스트 가져오기
+
+import { useInfiniteQuery } from "@tanstack/react-query";
+import GetCurrentUserPlayList from "../apis/playListApi";
+import type { GetCurrentUserPlayListRequest } from "../models/playList";
+
+const useGetCurrentUserPlayList = ({ limit, offset }: GetCurrentUserPlayListRequest) => {
+  return useInfiniteQuery({
+    queryKey: ["current-user-playlists"],
+    queryFn: ({ pageParam = 0 }) => {
+      return GetCurrentUserPlayList({ limit, offset: pageParam });
+    },
+
+    initialPageParam: 0,
+    getNextPageParam: (lastPage) => {
+      if (lastPage.next) {
+        const url = new URL(lastPage.next);
+        const nextOffset = url.searchParams.get("offset");
+
+        return nextOffset ? parseInt(nextOffset) : undefined;
+      }
+      return 10;
+    },
+  });
+};
+
+export default useGetCurrentUserPlayList;
