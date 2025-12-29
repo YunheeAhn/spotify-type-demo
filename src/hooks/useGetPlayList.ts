1743ef677526960b48275e62658cfccc9e131ed5
YunheeAhn <ayh95944674@gmail.com>
2025-12-29 21:10:13 +0900

    feat : 선택한 플레이리스트 가져오는 훅

---
+++ src/hooks/useGetPlayList.ts
@@ -0,0 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
+import { GetPlayListRequest } from "../models/playList";
+import { getPlayList } from "../apis/playListApi";
+
+// 선택한 플레이리스트 가져오는 훅
+const useGetPlayList = (params: GetPlayListRequest) => {
+  return useQuery({
+    queryKey: ["play-list-detail", params.playlist_id],
+    queryFn: () => {
+      return getPlayList(params);
+    },
+    // id값이 있을 때만 호출
+    enabled: !!params.playlist_id,
+  });
+};
+
+export default useGetPlayList;
