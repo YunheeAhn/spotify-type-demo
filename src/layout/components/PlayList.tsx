9a40be19da8fb2d04bd34c59cb284ea89cf63286
YunheeAhn <ayh95944674@gmail.com>
2025-12-29 18:53:40 +0900

    feat : 플레이리스트 카드 리스트 컴포넌트

---
+++ src/layout/components/PlayList.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router";
+import { SimplifiedPlayList } from "../../models/playList";
+import PlayListItem from "../../common/components/PlayListItem";
+
+interface PlaylistProps {
+  playlists: SimplifiedPlayList[];
+}
+
+// 사이드 바 플레이리스트 카드 리스트 컴포넌트
+const PlayList = ({ playlists }: PlaylistProps) => {
+  const [selectedId, setSelectedId] = useState<string>("");
+  const navigate = useNavigate();
+  const handleItemClick = (id: string) => {
+    setSelectedId(id);
+    navigate(`/playlist/${id}`);
+  };
+
+  return (
+    <div>
+      {playlists.map((item) => (
+        <PlayListItem
+          selected={selectedId === item.id}
+          handleClick={handleItemClick}
+          name={item.name || ""}
+          image={(item.images && item.images[0]?.url) || null}
+          id={item.id || ""}
+          key={item.id}
+          artistName={"Playlist •" + item.owner?.display_name}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default PlayList;
