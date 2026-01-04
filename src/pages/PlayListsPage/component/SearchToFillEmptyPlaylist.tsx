5def2065c8dfdca71573b9381696d7384ed167e2
YunheeAhn <ayh95944674@gmail.com>
2026-01-05 01:08:13 +0900

    feat : 빈 플레이리스트 채우기 위한 검색 컴포넌트

--- src/pages/PlayListsPage/component/SearchToFillEmptyPlaylist.tsx
+++ src/pages/PlayListsPage/component/SearchToFillEmptyPlaylist.tsx
@@ -1,25 +1,102 @@
-import { TextField, Typography } from "@mui/material";
-import React, { useState } from "react";
+import { Box, InputAdornment, styled, TextField, Typography } from "@mui/material";
+import React, { useRef, useState } from "react";
+import useSearchItemsByKeyword from "../../../hooks/useSearchItemsByKeyword";
+import { SearchType } from "../../../models/search";
+import SearchResultList from "./SearchResultList";
+import LoadingSpinner from "../../../common/components/LoadingSpinner";
+import SearchIcon from "@mui/icons-material/Search";
+import ErrorMessage from "../../../common/components/ErrorMessage";
 
-// 진행 중
-
+// 빈 플레이리스트 채우기 위한 검색 컴포넌트
 const SearchToFillEmptyPlaylist = () => {
   const [keyword, setKeyword] = useState<string>(""); // useState의 제네릭
+  const containerRef = useRef<HTMLDivElement | null>(null);
+
+  const { data, error, isLoading, hasNextPage, isFetchingNextPage, fetchNextPage } =
+    useSearchItemsByKeyword({
+      q: keyword,
+      type: [SearchType.Track],
+    });
+
+  if (error) {
+    // 에러가 발생했을 때 에러 메시지 표시
+    return <ErrorMessage message="Failed to Search" />;
+  }
+
   const handleSearchKeyword = (event: React.ChangeEvent<HTMLInputElement>) => {
     // React.ChangeEvent<HTMLInputElement> : input에서 오는 event의 타입
     // keyword를 입력하면 입력 값 보여주기
     setKeyword(event.target.value);
   };
 
+  const tracks = data?.pages.flatMap((page) => page.tracks?.items ?? []) ?? [];
+  const hasResults = tracks.length > 0;
+
   return (
-    <div>
+    <SearchContainer ref={containerRef}>
       <Typography variant="h1" my="10px">
         Let’s fill this playlist!
       </Typography>
 
-      <TextField value={keyword} onChange={handleSearchKeyword} />
-    </div>
+      <StyledTextField
+        autoComplete="off"
+        placeholder="Search for songs or artists"
+        value={keyword}
+        slotProps={{
+          input: {
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon style={{ color: "white" }} />
+              </InputAdornment>
+            ),
+          },
+        }}
+        onChange={handleSearchKeyword}
+      />
+      <Box mt={2}>
+        {isLoading ? (
+          // 검색 중일 때 로딩 스피너
+          <LoadingSpinner />
+        ) : hasResults ? (
+          // 검색 결과가 있을 때 결과 리스트 보여주기
+          <SearchResultList
+            list={tracks}
+            hasNextPage={hasNextPage}
+            isFetchingNextPage={isFetchingNextPage}
+            fetchNextPage={fetchNextPage}
+          />
+        ) : keyword === "" ? null : (
+          // 검색어가 비어 있을 때
+          // 검색 결과가 없을 때
+          <div>{`No Result for "${keyword}"`}</div>
+        )}
+      </Box>
+    </SearchContainer>
   );
 };
 
 export default SearchToFillEmptyPlaylist;
+
+const SearchContainer = styled(Box)({
+  padding: "16px",
+  width: "100%",
+  height: "100%",
+  overflowY: "auto",
+  "&::-webkit-scrollbar": { display: "none" },
+  msOverflowStyle: "none",
+  scrollbarWidth: "none",
+});
+
+const StyledTextField = styled(TextField)(({ theme }) => ({
+  width: "100%",
+  "& .MuiInputBase-root": {
+    borderRadius: "4px",
+    backgroundColor: theme.palette.action.active,
+    color: "white",
+  },
+  "& .MuiOutlinedInput-root": {
+    "& fieldset": { borderColor: "transparent" },
+    "&:hover fieldset": { borderColor: "gray" },
+    "&.Mui-focused fieldset": { borderColor: "gray" },
+  },
+}));
