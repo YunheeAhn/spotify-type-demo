87ca1d93dd722f8515de1bd6a997d6cef9e892e5
YunheeAhn <ayh95944674@gmail.com>
2026-01-09 20:41:45 +0900

    feat : 앨범, 트랙 컴포넌트 추가

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -3,11 +3,15 @@ import NewReleased from "./components/NewReleased";
 
 // 공통 스타일 컴포넌트
 import { Container } from "../../common/styles/commonStyled";
+import Albums from "./components/Albums";
+import Tracks from "./components/Tracks";
 
 const HomePage = () => {
   return (
     <Container>
       <NewReleased />
+      <Albums />
+      <Tracks />
     </Container>
   );
 };
